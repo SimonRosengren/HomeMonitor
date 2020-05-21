35940245c8c6881d52a20a147edad055ccc0fe01
simonrosengren <simonrosengren123@gmail.com>
2020-05-21 16:24:56 +0200

    merge fix

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const config = require('config')
 const dbPassword = config.get('mongoDb.mongoDbPassword')
 
-module.exports = asyncHandler( async () => {
+module.exports = async () => {
     try {
         console.log(dbPassword)
         await mongoose.connect(`mongodb://simon:${dbPassword}@ds151247.mlab.com:51247/homemonitor`, {
@@ -20,4 +20,4 @@ module.exports = asyncHandler( async () => {
         console.log(`Could not connect to db: ${error}`)
     }
 
-})
+}
--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,7 @@
 const errorHandler = require('../middleware/errorHandler')
 const tempRouter = require('../routes/temperature');
 const soilMoistureRouter = require('../routes/soilMoisture')
+const auth = require('../middleware/auth')
 module.exports = (app, express) => {
     app.use(express.json())
     app.use(auth)
