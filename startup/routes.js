9cc3de2fb1daebdbdc79d8140c3f701372819a4a
simonrosengren <simonrosengren123@gmail.com>
2020-05-22 22:17:50 +0200

    merge fix"

--- startup/routes.js
+++ startup/routes.js
@@ -4,7 +4,7 @@ const soilMoistureRouter = require('../routes/soilMoisture')
 const auth = require('../middleware/auth')
 module.exports = (app, express) => {
     app.use(express.json())
-    app.use(() => auth)
+    app.use(auth)
     app.use('/api/temperature', tempRouter)
     app.use('/api/soilmoisture', soilMoistureRouter)
     app.use(errorHandler)
