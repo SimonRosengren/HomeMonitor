6c3f9ef3c004cbedfd05cfff3b6ae2353d10a0c2
simonrosengren <simonrosengren123@gmail.com>
2020-06-11 15:06:35 +0200

    intercept OPTIONS

--- startup/routes.js
+++ startup/routes.js
@@ -8,7 +8,13 @@ module.exports = (app, express) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, application/json, Content-Length, X-Requested-With');
-        next();
+        // intercept OPTIONS method
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+        }
+        else {
+            next();
+        }
     });
     app.use(express.json())
     app.use(auth)
