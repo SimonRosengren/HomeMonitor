11fa0fe0ec31c1528634b7c95b7e360814e1af0b
simonrosengren <simonrosengren123@gmail.com>
2020-06-11 15:08:13 +0200

    whiote list x-api-key

--- startup/routes.js
+++ startup/routes.js
@@ -7,7 +7,7 @@ module.exports = (app, express) => {
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, application/json, Content-Length, X-Requested-With');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, application/json, x-api-key');
         // intercept OPTIONS method
         if ('OPTIONS' == req.method) {
             res.send(200);
