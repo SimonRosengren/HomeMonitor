440db9a63ba0871b4152f225878937ca04b72282
simonrosengren <simonrosengren123@gmail.com>
2020-06-11 14:32:00 +0200

    allow CORS

--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,12 @@ const tempRouter = require('../routes/temperature');
 const soilMoistureRouter = require('../routes/soilMoisture')
 const auth = require('../middleware/auth')
 module.exports = (app, express) => {
+    // Allow CORS
+    app.use((req, res) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    });
     app.use(express.json())
     app.use(auth)
     app.use('/api/temperature', tempRouter)
