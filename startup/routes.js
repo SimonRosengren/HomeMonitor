a65881730fbb82dd8755f902fd15f7313f35631a
simonrosengren <simonrosengren123@gmail.com>
2020-06-11 15:00:54 +0200

    white list application/json header

--- startup/routes.js
+++ startup/routes.js
@@ -7,7 +7,7 @@ module.exports = (app, express) => {
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, application/json, Content-Length, X-Requested-With');
         next();
     });
     app.use(express.json())
