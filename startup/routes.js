54aea9eb9033f120aace29fbfc05ebffd1dfc2b7
simonrosengren <simonrosengren123@gmail.com>
2020-06-11 14:46:56 +0200

    next after allowing CORS

--- startup/routes.js
+++ startup/routes.js
@@ -4,10 +4,11 @@ const soilMoistureRouter = require('../routes/soilMoisture')
 const auth = require('../middleware/auth')
 module.exports = (app, express) => {
     // Allow CORS
-    app.use((req, res) => {
+    app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        next();
     });
     app.use(express.json())
     app.use(auth)
