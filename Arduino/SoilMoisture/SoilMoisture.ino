e442c52758004880dce6a4290302890089cdeb28
simonrosengren <simonrosengren123@gmail.com>
2020-06-23 09:05:26 +0200

    combine arduino sketches

--- Arduino/SoilMoisture/SoilMoisture.ino
+++ Arduino/SoilMoisture/SoilMoisture.ino
@@ -1,12 +1,18 @@
 #include <ESP8266HTTPClient.h>
 #include "ESP8266WiFi.h"
+#include <dht11.h>
+#define DHT11PIN D7
 
 const int analogInPin = A0;
 int sensorValue = 0;
 const char *host = "http://simonrosengren.herokuapp.com/api/soilmoisture";
+const char *tempUrl = "http://simonrosengren.herokuapp.com/api/temperature";
+const char *humUrl = "http://simonrosengren.herokuapp.com/api/humidity";
 HTTPClient http;
 const char* ssid = "***";
 const char* password = "***";
+
+dht11 DHT11;
 
 void setup(void)
 { 
@@ -42,6 +48,36 @@ void loop() {
   Serial.println(httpCode);
   Serial.println(payload);
 
+  http.end();
+
+  int chk = DHT11.read(DHT11PIN);
+
+  float temp = (float)DHT11.temperature;
+  float hum = (float)DHT11.humidity;
+
+  postData = String("{\"date\":1,\"temperature\":") + temp + "}";
+  http.begin(tempUrl);
+  delay(1000); // See if this prevents the problm with connection refused and deep sleep
+  http.addHeader("Content-Type", "application/json");    
+  http.addHeader("x-api-key", "abcdefg123456789");    
+  
+  httpCode = http.POST(postData);
+  payload = http.getString();
+  Serial.println("Temp endpoint returned: ");
+  Serial.println(httpCode);
+  http.end();
+
+
+  postData = String("{\"date\":1,\"humidity\":") + hum + "}";
+  http.begin(humUrl);
+  delay(1000); // See if this prevents the problm with connection refused and deep sleep
+  http.addHeader("Content-Type", "application/json");    
+  http.addHeader("x-api-key", "abcdefg123456789");    
+  
+  httpCode = http.POST(postData);
+  payload = http.getString();
+  Serial.println("Hum endpoint returned: ");
+  Serial.println(httpCode);
   http.end();
 
   delay(1000 * 60);
